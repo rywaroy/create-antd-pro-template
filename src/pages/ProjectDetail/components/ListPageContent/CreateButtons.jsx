9fc41d02fccaa2791fd8ce8bfdf11231707a68de
roy <464485682@qq.com>
2022-06-15 11:08:56 +0800

    feat: 列表按钮配置

---
+++ src/pages/ProjectDetail/components/ListPageContent/CreateButtons.jsx
@@ -0,0 +1,88 @@
+import { Button, Popover } from 'antd';
+import {
+  ModalForm,
+  ProFormText,
+  ProFormSelect,
+} from '@ant-design/pro-components';
+import { PlusOutlined } from '@ant-design/icons';
+import styles from './index.less';
+
+const iconMap = {
+  PlusOutlined: <PlusOutlined />,
+};
+
+export default function CreateButtons(props) {
+  const { tableButtons, setTableButtons } = props;
+
+  const deleteButton = (index) => {
+    const newTableButtons = [...tableButtons];
+    newTableButtons.splice(index, 1);
+    setTableButtons(newTableButtons);
+  };
+
+  return (
+    <div className={styles.createButton}>
+      {tableButtons.map((item, index) => (
+        <Popover
+          key={index}
+          content={
+            <span
+              className={styles.createButtonDelete}
+              onClick={() => deleteButton(index)}
+            >
+              删除
+            </span>
+          }
+        >
+          <Button
+            type={item.type}
+            icon={item.icon}
+            className={styles.createButtonItem}
+          >
+            {item.text}
+          </Button>
+        </Popover>
+      ))}
+
+      <ModalForm
+        title="新建按钮"
+        width={400}
+        trigger={<Button icon={<PlusOutlined />}></Button>}
+        autoFocusFirstInput
+        onFinish={(values) => {
+          const { text, type = 'default', icon } = values;
+          const buttons = {
+            text,
+            type,
+          };
+          if (icon) {
+            buttons.icon = iconMap[icon];
+            buttons.iconText = `<${icon} />`;
+          }
+          setTableButtons([...tableButtons, buttons]);
+          return true;
+        }}
+      >
+        <ProFormText
+          name="text"
+          label="按钮文本"
+          rules={[{ required: true, message: '请输入按钮文本' }]}
+        />
+        <ProFormSelect
+          name="type"
+          label="按钮类型"
+          initialValue={'primary'}
+          options={[
+            { value: 'primary', label: '主要 primary' },
+            { value: 'default', label: '默认 default' },
+          ]}
+        />
+        <ProFormSelect
+          name="icon"
+          label="按钮图标"
+          options={[{ value: 'PlusOutlined', label: '+' }]}
+        />
+      </ModalForm>
+    </div>
+  );
+}
